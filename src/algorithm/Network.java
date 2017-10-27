55b69d0fec64c1f6069cb7c6c80278bfc8fad898
Elliott Coleman <ejaycoleman@googlemail.com>
2017-10-27 10:57:59 +0100

    private vars declared. Started initialisation

--- src/algorithm/Network.java
+++ src/algorithm/Network.java
@@ -1,7 +1,31 @@
 package algorithm;
 
 public class Network {
+	private int ni;
+	private int nh;
+	private int no;
+	
+	private Double[] ai;
+	private int ah;
+	private int ao;
+	
+	private int wi;
+	private int wo;
+	
+	private int ci;
+	private int co;
+	
+
 	public Network(int ni, int nh, int no) {
+		// number of input, hidden and output nodes
+		this.ni = ni + 1;
+		this.nh = nh;
+		this.no = no;
+		
+		this.ai = new Double[ni];
+		for(int i=0; i < ni; i++){
+		    this.ai[i] = 1.0;
+		}
 		
 	}
 }
