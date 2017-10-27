f3192115d41beefeb11c48292e1215ede2627678
Elliott Coleman <ejaycoleman@googlemail.com>
2017-10-27 19:19:04 +0100

    Constructor built. Create matrix function in development

--- src/algorithm/Network.java
+++ src/algorithm/Network.java
@@ -6,8 +6,8 @@ public class Network {
 	private int no;
 	
 	private Double[] ai;
-	private int ah;
-	private int ao;
+	private Double[] ah;
+	private Double[] ao;
 	
 	private int wi;
 	private int wo;
@@ -15,6 +15,16 @@ public class Network {
 	private int ci;
 	private int co;
 	
+	/*private void makeMatrix(int I, int J, int fill) {
+	    Double [] m;
+	    
+	    for (int i=0; i < I; i++) {
+	    	    m.add([fill]*J);
+	    	}
+	    	
+	    	return m
+	    	    		
+	}*/
 
 	public Network(int ni, int nh, int no) {
 		// number of input, hidden and output nodes
@@ -23,9 +33,42 @@ public class Network {
 		this.no = no;
 		
 		this.ai = new Double[ni];
-		for(int i=0; i < ni; i++){
+		this.ah = new Double[nh];
+		this.ao = new Double[no];
+		
+		for (int i=0; i < ni; i++) {
 		    this.ai[i] = 1.0;
 		}
+		
+		for (int i=0; i < nh; i++) {
+		    this.ah[i] = 1.0;
+		}
+		
+		for (int i=0; i < no; i++) {
+		    this.ao[i] = 1.0;
+		}
+		
+		/*
+		 * 
+        this.wi = makeMatrix(this.ni, this.nh)
+        this.wo = makeMatrix(this.nh, this.no)
+
+        for (int i=0; i < this.ni; i++) {
+            for (int j=0; j < this.nh; j++) {
+                this.wi[i][j] = rand(-0.2, 0.2)
+            }
+        }
+        
+        for (int j=0; j < this.nh; j++) {
+        		for (int k=0; k < this.no; k++) {
+                this.wo[j][k] = rand(-2.0, 2.0)
+            	}
+        }
+
+        this.ci = makeMatrix(this.ni, this.nh)
+        this.co = makeMatrix(this.nh, this.no)
+        
+		*/
 		
 	}
 }
